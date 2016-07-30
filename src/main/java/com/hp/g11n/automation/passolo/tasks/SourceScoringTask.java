675197ba8f671cd28d8b58b835335e1aa541e0d9
foy <ljs2342003@gmail.com>
2016-07-30 15:45:12 +0800

    foobar

---
+++ src/main/java/com/hp/g11n/automation/passolo/tasks/SourceScoringTask.java
@@ -0,0 +1,74 @@
+package com.hp.g11n.automation.passolo.tasks;
+
+
+import com.hp.g11n.sdl.psl.interop.core.*;
+import com.hp.g11n.sdl.psl.interop.core.impl.impl.PassoloApp;
+import javafx.concurrent.Task;
+import javafx.scene.control.TextArea;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.FilenameFilter;
+import java.util.HashSet;
+import java.util.Set;
+
+public class SourceScoringTask  extends Task<Void> {
+    private final static Set<String> KEY_WORDS=new HashSet<String>() {{
+            String  keys="of, is, are, if, by, with, in, at, on, from, to, and, " +
+                    "or, for, an, a, the, where, which, when, but, this, that, over, " +
+                    "under, after, since";
+            for(String k:keys.split(",")){
+                add(k.toLowerCase().trim());
+            }
+        }};
+    private String source;
+    private String report;
+    private TextArea log;
+    public SourceScoringTask(String sourceDir,String reportDir,TextArea log){
+        this.source=sourceDir;
+        this.report=reportDir;
+        this.log=log;
+    }
+
+    @Override
+    protected Void call() throws Exception {
+        //input
+        File[] lpuFiles = (new File(source)).listFiles((File f) -> !f.isDirectory() && f.getName().endsWith(".lpu"));
+        File lpuFile=lpuFiles[0];
+
+        //output
+        FileWriter fw=new FileWriter(report+"/"+System.currentTimeMillis()+".csv");
+
+        // Create a Passolo instance.
+        IPassoloApp app = PassoloApp.getInstance();
+        // Open an lpu file to load a Passolo project.
+        IPslProject project = app.open(lpuFile.toPath());
+        // Get translation lists of the project
+        log.appendText("starting process source lists...");
+        IPslSourceLists sourceLists = project.getSourceLists();
+        for(IPslSourceList sourceList : sourceLists.toList()) {
+            for(IPslSourceString sourceString : sourceList.getSourceStrings()){
+                String str=sourceString.getText();
+                String target=str.toLowerCase().trim();
+                if(target.isEmpty()){
+                    continue;
+                }
+                //just for example...
+                for(String k: KEY_WORDS){
+                    if(target.contains(k)){
+                        String tmp=sourceString.getID()+","+sourceString.getText();
+                        log.appendText(tmp);
+                        fw.write(tmp);
+                        break;
+                    }
+                }
+            }
+        }
+        log.appendText("process source lists done...");
+        // Close the project.
+        project.close();
+        // Shut down passolo instance.
+        IPassoloApp.quit();
+        return null;
+    }
+}
--- src/main/java/com/hp/g11n/scoring/Main.java
+++ src/main/java/com/hp/g11n/scoring/Main.java
@@ -2,15 +2,19 @@ package com.hp.g11n.scoring;
 
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.geometry.Insets;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.StackPane;
+import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
+
+import java.io.File;
 
 
 public class Main extends Application {
@@ -22,7 +26,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Source Scoring!");
         Group root = new Group();
-        Scene scene = new Scene(root, 300, 150);
+        Scene scene = new Scene(root, 350, 200);
         primaryStage.setScene(scene);
 
         GridPane grid = new GridPane();
@@ -32,39 +36,68 @@ public class Main extends Application {
 
         scene.setRoot(grid);
 
+        //FileChooser
+        DirectoryChooser chooser=new DirectoryChooser();
+
         //source file
         Label label1 = new Label("File to be processed:*:");
+        Button browser1 = new Button("Browse...");
+
         final TextField sourceFile = new TextField();
-        sourceFile.setPromptText("");
-        GridPane.setConstraints(label1,0 , 0);
+        sourceFile.setText("c:/tmp/psl-generate-sorucescoring-report");
+        browser1.setOnAction((final ActionEvent e) -> {
+            File file = chooser.showDialog(primaryStage);
+            if (file != null) {
+                sourceFile.setText(file.getAbsolutePath());
+            }
+        });
+        GridPane.setConstraints(label1, 0, 0);
         GridPane.setConstraints(sourceFile, 1, 0);
-        grid.getChildren().addAll(label1,sourceFile);
+        GridPane.setConstraints(browser1, 2, 0);
+        grid.getChildren().addAll(label1,sourceFile,browser1);
 
         //exclusion file path
         Label label2 = new Label("Exclusion File Path:");
+        Button browser2 = new Button("Browse...");
         final TextField exclusionFile = new TextField();
         exclusionFile.setPromptText("");
         GridPane.setConstraints(label2, 0, 1);
         GridPane.setConstraints(exclusionFile, 1, 1);
-        grid.getChildren().addAll(label2,exclusionFile);
+        GridPane.setConstraints(browser2, 2, 1);
+        grid.getChildren().addAll(label2,exclusionFile,browser2);
 
         //output folder
         Label label3 = new Label("Write Output to the folder:");
+        Button browser3 = new Button("Browse...");
         final TextField outputFolder = new TextField();
-        outputFolder.setPromptText("");
+        outputFolder.setText("c:/tmp/psl-generate-sorucescoring-report");
+        browser3.setOnAction((final ActionEvent e) -> {
+            File file = chooser.showDialog(primaryStage);
+            if (file != null) {
+                outputFolder.setText(file.getAbsolutePath());
+            }
+        });
         GridPane.setConstraints(label3, 0, 2);
         GridPane.setConstraints(outputFolder, 1, 2);
-        grid.getChildren().addAll(label3,outputFolder);
+        GridPane.setConstraints(browser3, 2, 2);
+        grid.getChildren().addAll(label3, outputFolder, browser3);
 
         //button
         Button submit = new Button("Run");
-        GridPane.setConstraints(submit, 0, 3);
+        GridPane.setConstraints(submit, 1, 3);
         grid.getChildren().add(submit);
 
         Button clear = new Button("Cancel");
-        GridPane.setConstraints(clear, 1, 3);
+        GridPane.setConstraints(clear, 2, 3);
         grid.getChildren().add(clear);
 
+        //log textArea
+        Label label4 = new Label("logs:");
+        TextArea log = new TextArea();
+        log.appendText("log will display here!");
+        GridPane.setConstraints(label4,0,4);
+        GridPane.setConstraints(log,1,4);
+        grid.getChildren().addAll(label4,log);
         primaryStage.show();
     }
 }
